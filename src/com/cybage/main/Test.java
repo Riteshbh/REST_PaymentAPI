eddf0fb339ca236a4b65f50a10926583cbbff4ed
anupa <anupa@DESKTOP-253BSI8>
2022-12-02 15:55:38 +0530

    Hi This is demo

---
+++ src/com/cybage/main/Test.java
@@ -0,0 +1,9 @@
+package com.cybage.main;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hi This is a Test Demo");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author anupa
+ *
+ */
+module PaymentAPIs {
+}
