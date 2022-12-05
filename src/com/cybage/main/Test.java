609bc37e4a63b38e3c445af4b832633e96d97c38
Ritesh <riteshbh@cybage.com>
2022-12-05 11:32:48 +0530

    commit from cybage machine

--- src/com/cybage/main/Test.java
+++ src/com/cybage/main/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("Hi This is a Test Demo");
 		System.out.println("Hi I am Ritesh Added Repo");
 		System.out.println("Hi I am Anupam Added Response for Ritesh Bro");
+		System.out.println("From cybage machine");
 	}
 
 }
